56803efcd0316e69221e05b15c6f22c8edf0a0a3
Augusto Andrade <gutoandrade01@gmail.com>
2025-03-15 20:30:06 -0300

    Implementa logica para mudar status para foco e validação do token

--- src/main/java/dev/wakandaacademy/produdoro/usuario/application/api/UsuarioController.java
+++ src/main/java/dev/wakandaacademy/produdoro/usuario/application/api/UsuarioController.java
@@ -2,6 +2,9 @@ package dev.wakandaacademy.produdoro.usuario.application.api;
 
 import javax.validation.Valid;
 
+import dev.wakandaacademy.produdoro.handler.APIException;
+import dev.wakandaacademy.produdoro.config.security.service.TokenService;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +20,8 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class UsuarioController implements UsuarioAPI {
 	private final UsuarioService usuarioAppplicationService;
+	private final TokenService tokenService;
+
 
 	@Override
 	public UsuarioCriadoResponse postNovoUsuario(@Valid UsuarioNovoRequest usuarioNovo) {
@@ -32,5 +37,18 @@ public class UsuarioController implements UsuarioAPI {
 		UsuarioCriadoResponse buscaUsuario = usuarioAppplicationService.buscaUsuarioPorId(idUsuario);
 		log.info("[finaliza] UsuarioController - buscaUsuarioPorId");
 		return buscaUsuario;
+	}
+
+	@Override
+	public void mudaStatusParaFoco(String token, UUID idUsuario) {
+		log.info("[inicia] UsuarioController - alteraStatusParaFoco");
+		String usuario = validaTokenUsuario(token); // Validação do token é feita aqui
+		usuarioAppplicationService.mudaStatusParaFoco(usuario, idUsuario);
+		log.info("[finaliza] UsuarioController - alteraStatusParaFoco");
+	}
+
+	private String validaTokenUsuario(String token) {
+		return tokenService.getUsuarioByBearerToken(token)
+				.orElseThrow(() -> APIException.build(HttpStatus.UNAUTHORIZED, "Credencial de autenticação não é válida"));
 	}
 }

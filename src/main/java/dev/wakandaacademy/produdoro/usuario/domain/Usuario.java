1cc6cac7a372b843eb040eb091ac734a48d5188d
OliveiraGomesMoises <mosesgdo@gmail.com>
2025-03-18 13:42:47 -0300

    feat:implementa camada domain

--- src/main/java/dev/wakandaacademy/produdoro/usuario/domain/Usuario.java
+++ src/main/java/dev/wakandaacademy/produdoro/usuario/domain/Usuario.java
@@ -4,6 +4,8 @@ import java.util.UUID;
 
 import javax.validation.constraints.Email;
 
+import dev.wakandaacademy.produdoro.handler.APIException;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -16,6 +18,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.http.HttpStatus;
 
 @Builder
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -23,25 +26,51 @@ import lombok.ToString;
 @Getter
 @ToString
 @Document(collection = "Usuario")
+@Log4j2
 public class Usuario {
-	@Id
-	private UUID idUsuario;
-	@Email
-	@Indexed(unique = true)
-	private String email;
-	private ConfiguracaoUsuario configuracao;
-	@Builder.Default
-	private StatusUsuario status = StatusUsuario.FOCO;
-	@Builder.Default
-	private Integer quantidadePomodorosPausaCurta = 0;
-	
-	public Usuario(UsuarioNovoRequest usuarioNovo, ConfiguracaoPadrao configuracaoPadrao) {
-		this.idUsuario = UUID.randomUUID();
-		this.email = usuarioNovo.getEmail();
-		this.status = StatusUsuario.FOCO;
-		this.configuracao = new ConfiguracaoUsuario(configuracaoPadrao);
-	}
+    @Id
+    private UUID idUsuario;
+    @Email
+    @Indexed(unique = true)
+    private String email;
+    private ConfiguracaoUsuario configuracao;
+    @Builder.Default
+    private StatusUsuario status = StatusUsuario.FOCO;
+    @Builder.Default
+    private Integer quantidadePomodorosPausaCurta = 0;
+
+    public Usuario(UsuarioNovoRequest usuarioNovo, ConfiguracaoPadrao configuracaoPadrao) {
+        this.idUsuario = UUID.randomUUID();
+        this.email = usuarioNovo.getEmail();
+        this.status = StatusUsuario.FOCO;
+        this.configuracao = new ConfiguracaoUsuario(configuracaoPadrao);
+    }
 
     public void mudaStatusParaPausaLonga(UUID idUsuario) {
+		log.info("[inicia] usuario - mudaStatusParaPausaLonga");
+        validaUsuario(idUsuario);
+        validaSeUsuarioJaEstarEmPausaLonga();
+        this.status = StatusUsuario.PAUSA_LONGA;
+        log.info("[finaliza] usuario - mudaStatusParaPausaLonga");
+
+    }
+
+    private void validaSeUsuarioJaEstarEmPausaLonga() {
+        log.info("[inicia] usuario - validaSeUsuarioJaEstarEmPausaLonga");
+        if (this.status.equals(StatusUsuario.PAUSA_LONGA)){
+            log.info("[finaliza] APIExeception - validaSeUsuarioJaEstarEmPausaLonga");
+            throw APIException.build(HttpStatus.BAD_REQUEST, "Usuario ja eata em PAUSA LONGA");
+        }
+        log.info("[finaliza] usuario - validaSeUsuarioJaEstarEmPausaLonga");
+
+    }
+
+    private void validaUsuario(UUID idUsuario) {
+        log.info("[inicia] usuario - validaUsuario");
+        if (this.idUsuario.equals(idUsuario)){
+            log.info("[finaliza] APIExeception - validaUsuario");
+            throw  APIException.build(HttpStatus.UNAUTHORIZED, "credencial de autenticação não é válida.");
+        }
+        log.info("[finaliza] usuario - validaUsuario");
     }
 }

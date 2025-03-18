8c2582dafe547829fcae96719323ffbcb9264174
OliveiraGomesMoises <mosesgdo@gmail.com>
2025-03-18 20:29:10 -0300

    test:implementa test unitário

--- src/test/java/dev/wakandaacademy/produdoro/usuario/application/service/UsuarioApplicationServiceTest.java
+++ src/test/java/dev/wakandaacademy/produdoro/usuario/application/service/UsuarioApplicationServiceTest.java
@@ -1,7 +1,35 @@
 package dev.wakandaacademy.produdoro.usuario.application.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import dev.wakandaacademy.produdoro.DataHelper;
+import dev.wakandaacademy.produdoro.usuario.application.repository.UsuarioRepository;
+import dev.wakandaacademy.produdoro.usuario.domain.StatusUsuario;
+import dev.wakandaacademy.produdoro.usuario.domain.Usuario;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class UsuarioApplicationServiceTest {
+    @InjectMocks
+    UsuarioApplicationService usuarioApplicationService;
 
+    @Mock
+    UsuarioRepository usuarioRepository;
+
+    @Test
+    void deveMudarParaPausaLonga() {
+        Usuario usuario = DataHelper.createUsuario1();
+        when(usuarioRepository.buscaUsuarioPorEmail(anyString())).thenReturn(usuario);
+        when(usuarioRepository.buscaUsuarioPorId(any())).thenReturn(usuario);
+        usuarioApplicationService.mudaStatusPausaLonga(usuario.getEmail(), usuario.getIdUsuario());
+        assertEquals(StatusUsuario.PAUSA_LONGA, usuario.getStatus());
+        verify(usuarioRepository, times(1)).salva(usuario);
+    }
 }
